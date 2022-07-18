246983e0474c650f8a11a3bb13032dfddf4a349e
Rosario Lucas <lucasrosario1@hotmail.com>
2022-07-18 20:06:16 -0300

    Exibindo e manipulando dados da API

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -15,10 +17,18 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+       
 
-        // pegar só os dados que interessam (Titul, poste , classificação)
-
+        // extrair só os dados que interessam (Titul, poste , classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parser(body);
+            
         // exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
