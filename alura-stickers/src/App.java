fb469b94ae51aadcdaece680b100395b82097226
Rosario Lucas <lucasrosario1@hotmail.com>
2022-07-21 09:21:12 -0300

    Iniciando API Nasa

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -14,7 +14,8 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_qp3qnxqp";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+        String url ="https://api.nasa.gov/planetary/apod?api_key=0Ut16XcLFjXw3vgXMEx4cwH4IWr1wLVtSvi2O8PU&start_date=2022-06-12&end_date=2022-06-14";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -23,22 +24,26 @@ public class App {
 
         // extrair só os dados que interessam (Titul, poste , classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parser(body);
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String, String> filme : listaDeFilmes) {
-            String urlImage = filme.get("image");
+        for (int i = 0; i < 3; i++) {
+            Map<String, String> filme = listaDeFilmes.get(i);
+
+            //String urlImage = filme.get("image");
+            String urlImage = filme.get("url").replaceAll("(@+)(.*).jpg$", "$1.jpg");
             String titulo = filme.get("title");
 
             InputStream inputStream = new URL(urlImage).openStream();
-
-            String nomeArquivo = titulo + ".png";
+            String nomeArquivo ="saida/" + titulo + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
             System.out.println(titulo);
             System.out.println();
         }
+       
+        
     }
 }
